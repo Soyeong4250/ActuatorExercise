69a815401c08d44d931da166c8e6c966f7ffe928
이소영 <th42500@naver.com>
2023-03-13 22:48:59 +0900

    [Feat] JVM 메모리 사용량 초과인 경우 모니터링
    👉 Grafana를 이용하여 JVM 사용량이 초과인 경우의 모니터링 결과 JVM Heap 사용량이 증가하는 것을 확인할 수 있음
    👉 JVM 사용량이 초과되었을 때 인텔리제이 콘솔에서 OutOfMemoryError가 발생을 확인할 수 있음

--- src/main/java/hello/controller/TrafficController.java
+++ src/main/java/hello/controller/TrafficController.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
 @RestController
 public class TrafficController {
@@ -16,5 +19,15 @@ public class TrafficController {
             value++;
         }
         return "ok value = " + value;
+    }
+
+    private List<String> list = new ArrayList<>();  // 누적을 위한 List 선언
+    @GetMapping("/jvm")
+    public String jvm() {
+        log.info("jvm");
+        for (int i = 0; i < 1000000; i++) {
+            list.add("hello jvm!" + i);
+        }
+        return "ok jvm";
     }
 }
